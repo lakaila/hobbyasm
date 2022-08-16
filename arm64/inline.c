9c8fda60c89319d9120aeda4d69d1defa27d1485
Lauri Kaila <lauri.kaila@gmail.com>
2022-08-16 21:55:05 +0300

    Try out crc32h, crc32w, crc32x

--- arm64/inline.c
+++ arm64/inline.c
@@ -86,6 +86,63 @@ uint32_t crc32bin(const char *ptr, size_t len)
     return crc ^ 0xFFFFFFFF;
 }
 
+uint32_t crc32bin_16(const char *ptr, size_t len)
+{
+    // process 16 bytes at a time (halfwords)
+    // address must be aligned! size must match!
+    uint32_t crc = 0xFFFFFFFF;
+    asm("cmp %[LEN], xzr;"
+        "b 1f;"
+        "2: subs %[LEN], %[LEN], #2;"
+        "1: b.eq 3f;"
+        "ldrh w1, [%[P]], #2;"
+        "crc32h %w[CRC], %w[CRC], w1;"
+        "b 2b;"
+        "3:"
+        : [CRC] "+r"(crc), [P] "+r"(ptr), [LEN] "+r"(len)
+        : "m"(*(const char(*)[])ptr)
+        : "w1");
+    return crc ^ 0xFFFFFFFF;
+}
+
+uint32_t crc32bin_32(const char *ptr, size_t len)
+{
+    // process 32 bytes at a time (words)
+    // address must be aligned! size must match!
+    uint32_t crc = 0xFFFFFFFF;
+    asm("cmp %[LEN], xzr;"
+        "b 1f;"
+        "2: subs %[LEN], %[LEN], #4;"
+        "1: b.eq 3f;"
+        "ldr w1, [%[P]], #4;"
+        "crc32w %w[CRC], %w[CRC], w1;"
+        "b 2b;"
+        "3:"
+        : [CRC] "+r"(crc), [P] "+r"(ptr), [LEN] "+r"(len)
+        : "m"(*(const char(*)[])ptr)
+        : "w1");
+    return crc ^ 0xFFFFFFFF;
+}
+
+uint32_t crc32bin_64(const char *ptr, size_t len)
+{
+    // process 64 bytes at a time (doublewords)
+    // address must be aligned! size must match!
+    uint32_t crc = 0xFFFFFFFF;
+    asm("cmp %[LEN], xzr;"
+        "b 1f;"
+        "2: subs %[LEN], %[LEN], #8;"
+        "1: b.eq 3f;"
+        "ldr x1, [%[P]], #8;"
+        "crc32x %w[CRC], %w[CRC], x1;"
+        "b 2b;"
+        "3:"
+        : [CRC] "+r"(crc), [P] "+r"(ptr), [LEN] "+r"(len)
+        : "m"(*(const char(*)[])ptr)
+        : "w1");
+    return crc ^ 0xFFFFFFFF;
+}
+
 void breakpoint()
 {
     asm("brk #55");
@@ -103,8 +160,14 @@ int main()
     printf("crc32c(aaa)=%X\n", crc32c("aaa"));
     printf("crc32(aaa)=%X\n", crc32("aaa"));
     printf("crc32(Test)=%X\n", crc32("Test"));
+    printf("crc32(TestTest)=%X\n", crc32("TestTest"));
     printf("crc32c(Test)=%X\n", crc32c("Test"));
     printf("crc32bin(Test)=%X\n", crc32bin("Test", 4));
     printf("crc32(1234567890)=%X crc32c=%x\n", crc32("1234567890"), crc32c("1234567890"));
     printf("crc32(The quick..)=%X\n", crc32("The quick brown fox jumps over the lazy dog"));
+    printf("crc32bin_16(Test)=%X\n", crc32bin_16("Test", 4));
+    printf("crc32bin_32(Test)=%X\n", crc32bin_32("Test", 4));
+    printf("crc32bin_16(TestTest)=%X\n", crc32bin_16("TestTest", 8));
+    printf("crc32bin_32(TestTest)=%X\n", crc32bin_32("TestTest", 8));
+    printf("crc32bin_64(TestTest)=%X\n", crc32bin_64("TestTest", 8));
 }
